3a136fb1403e95c3289185c09463b9bbeebaba13
George Huliaris <georgehuliaris@me.com>
2021-04-27 19:14:57 -0500

    added to server.js

---
+++ server.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const mongojs = require("mongojs");
+const mongoose = require('mongoose');
+const app = express();
+const Schema = mongoose.Schema;
+const ObjectId = Schema.ObjectId;
