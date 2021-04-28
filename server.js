9c264c859e6cb2c1342f8c33f40473d7515fe15e
George Huliaris <georgehuliaris@me.com>
2021-04-27 20:53:05 -0500

    added to server.js

--- server.js
+++ server.js
@@ -4,3 +4,12 @@ const mongoose = require('mongoose');
 const app = express();
 const Schema = mongoose.Schema;
 const ObjectId = Schema.ObjectId;
+require("dotenv").config()
+
+const PORT = process.env.PORT || 3000
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutDB", { useNewUrlParser: true }, { useUnifiedTopology: true });
+
+app.listen(3000, () => {
+    console.log(`App running on port ${PORT}!`);
+  });
